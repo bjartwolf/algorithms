85ca86409380fee86218ae19f799f9ad6d2b9a3a
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-27 21:14:51 +0100

    Update Program.fs

--- Dijkstra/Program.fs
+++ Dijkstra/Program.fs
@@ -10,18 +10,18 @@ module PQ = PriorityQueue
 type Edge = { 
     u : Node;
     v : Node;
-    ω_u_v : int;
+    ω_uv : int;
 }
 type Edges = Edge Set
 let exampleNodes = Set.ofList [ {Id = 1}; {Id = 2}; {Id = 3}; {Id = 4}; {Id = 5}; {Id = 6} ]
-let exampleEdges = Set.ofList [ { u = {Id = 1}; v = {Id = 2}; ω_u_v = 7 }; 
-                                { u = {Id = 1}; v = {Id = 3}; ω_u_v = 9 }; 
-                                { u = {Id = 2}; v = {Id = 3}; ω_u_v = 10 }; 
-                                { u = {Id = 2}; v = {Id = 4}; ω_u_v = 15 }; 
-                                { u = {Id = 3}; v = {Id = 4}; ω_u_v = 11 }; 
-                                { u = {Id = 3}; v = {Id = 6}; ω_u_v = 2 }; 
-                                { u = {Id = 4}; v = {Id = 5}; ω_u_v = 6 }; 
-                                { u = {Id = 5}; v = {Id = 6}; ω_u_v = 9 } ]
+let exampleEdges = Set.ofList [ { u = {Id = 1}; v = {Id = 2}; ω_uv = 7 }; 
+                                { u = {Id = 1}; v = {Id = 3}; ω_uv = 9 }; 
+                                { u = {Id = 2}; v = {Id = 3}; ω_uv = 10 }; 
+                                { u = {Id = 2}; v = {Id = 4}; ω_uv = 15 }; 
+                                { u = {Id = 3}; v = {Id = 4}; ω_uv = 11 }; 
+                                { u = {Id = 3}; v = {Id = 6}; ω_uv = 2 }; 
+                                { u = {Id = 4}; v = {Id = 5}; ω_uv = 6 }; 
+                                { u = {Id = 5}; v = {Id = 6}; ω_uv = 9 } ]
 
 type DistanceNode = int * Node
 
@@ -54,7 +54,7 @@ let adjacent u e =
 
 let relax adjacent (d_u: int) explored = 
     adjacent 
-      |> Set.map (fun u_v -> d_u + u_v.ω_u_v, u_v.v)
+      |> Set.map (fun u_v -> d_u + u_v.ω_uv, u_v.v)
       |> Set.filter (fun dn -> closerOrUndiscovered explored dn) 
 
 //https://web.engr.oregonstate.edu/~glencora/wiki/uploads/dijkstra-proof.pdf
@@ -64,9 +64,9 @@ let dijkstra edges n =
    let rec innerDijkstra R explored =  
         match (PQ.tryPop R) with
             | None  -> explored 
-            | Some ((d_v,u), R') ->  
+            | Some ((d_u,u), R') ->  
                 let adjacent = adjacent u edges
-                let relaxed = relax adjacent d_v explored
+                let relaxed = relax adjacent d_u explored
                 innerDijkstra (updateFrontier R' relaxed) 
                               (updateDiscovered explored relaxed)
    let pq = PQ.empty false |> PQ.insert (0, n)
@@ -79,7 +79,7 @@ let vertexCount = int romeData.[0]
 let edgeCount = int romeData.[1]
 let romeEdges = romeData.[2..]
                     |> Array.map (fun line -> line.Split(' '))
-                    |> Array.map (fun line -> { u = {Id = int line.[0]}; v = {Id = int line.[1]}; ω_u_v = int line.[2] })
+                    |> Array.map (fun line -> { u = {Id = int line.[0]}; v = {Id = int line.[1]}; ω_uv = int line.[2] })
                     |> Set.ofArray
                
 [<Xunit.Fact>]
